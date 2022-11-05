2205b3dc926b501cf311e8571d694c136d00eedd
Kidmain <thekidmain@gmail.com>
2022-11-05 11:03:28 +0300

    Add methods for Application.class + comments of this class in the beginning.

--- src/main/java/com/kidmain/Application.java
+++ src/main/java/com/kidmain/Application.java
@@ -6,11 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/**
+ * 1. getWordsList() – load the file {words_aplha.txt} and add word to List.
+ * 2. getRandomWord() – get a random word from the {wordsList}.
+ * 3. printGameState() - print a current game state (guessed and unguessed letters).
+ * 4. isAllLettersGuessed() – return TRUE if all letters of {guessingWord} are guessed.
+ * 5. isWordGuessed() – return TRUE if player guessed the {guessingWord}.
+ * 6. getPlayerLetterGuess() – ask player write a letter.
+ * 7. getPlayerWordGuess() – ask player write a word.
+ * 8. start() – public method to start the game.
+ */
 
 public class Application {
     private final String pathOfWords = "words_alpha.txt";
-    private final String correctWord = getRandomWord();
-    private final List<Character> playerGuesses = new ArrayList<>();
+    private final String guessingWord = getRandomWord();
+    private final List<Character> playerLetterGuesses = new ArrayList<>();
+    private String playerWordGuess = "";
+    private final String victoryText = "==========\nCONGRATULATIONS!!! YOU HAVE GUESSED THE WORD :)\n==========";
 
     private List<String> getWordsList() {
         Scanner scanner;
@@ -33,10 +47,11 @@ public class Application {
         return wordsList.get(random.nextInt(wordsList.size()));
     }
 
-    private void printWordState() {
-        for (int i = 0; i < correctWord.length(); i++) {
-            if (playerGuesses.contains(correctWord.charAt(i))) {
-                System.out.print(correctWord.charAt(i));
+    private void printGameState() {
+        char[] guessingWordCharArray = guessingWord.toCharArray();
+        for (char ch : guessingWordCharArray) {
+            if (playerLetterGuesses.contains(ch)) {
+                System.out.print(ch);
             } else {
                 System.out.print("-");
             }
@@ -44,14 +59,57 @@ public class Application {
         System.out.println();
     }
 
-    public void getPlayerGuess() {
+    private boolean isAllLettersGuessed() {
+        int guessingWordLength = guessingWord.length();
+        int guessedLettersCount = (int) IntStream
+                .range(0, guessingWordLength)
+                .filter(i -> playerLetterGuesses.contains(guessingWord.charAt(i)))
+                .count();
+
+        return guessedLettersCount == guessingWordLength;
+    }
+
+
+    private boolean isWordGuessed() {
+        return playerWordGuess.equals(guessingWord);
+    }
+
+    private void getPlayerLetterGuess() {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Please enter a letter: ");
-
         String letterGuess = scanner.nextLine();
-        playerGuesses.add(letterGuess.charAt(0));
 
-        printWordState();
+        playerLetterGuesses.add(letterGuess.charAt(0));
+    }
+
+    private void getPlayerWordGuess() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your guess for the word: ");
+        playerWordGuess = scanner.nextLine();
+    }
+
+    public void start() {
+        while (true) {
+            getPlayerLetterGuess();
+            if (isAllLettersGuessed()) {
+                System.out.println(victoryText);
+                break;
+            }
+
+            printGameState();
+
+            getPlayerWordGuess();
+            if (isWordGuessed()) {
+                System.out.println(victoryText);
+                break;
+            } else {
+                System.out.println("Nope :C Nice try.");
+            }
+
+            printGameState();
+        }
     }
 
 }
--- src/main/java/com/kidmain/Game.java
+++ src/main/java/com/kidmain/Game.java
@@ -1,20 +1,8 @@
 package com.kidmain;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
-
-/**
- * 1. Load the file {words_aplha.txt}.
- * 2. Read from this file using loop and add words to list.
- * 3. Get a random word from the {wordsList}.
- */
-
 public class Game {
     public static void main(String[] args) {
-        Application app = new Application();
+        Application game = new Application();
+        game.start();
     }
 }
